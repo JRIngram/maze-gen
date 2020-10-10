2f7e1c57ecbc78c54232d931cd50a4edae67cf22
J. Ingram <JRIngram@users.noreply.github.com>
2020-10-10 15:13:32 +0100

    Add further error handling (#54)
    * Add further tests
    
    * Further test coverage
    
    * Fix documentation
    
    * Fix linting issues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+* Additional error handling
+* New tests
+
+### Changes
+* New max dimensions, to stop heap out of memory errors
+* Minor documentation fixes
 
 ## [2.0.0] - 2020-04-21
 ### Added
--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const Generator = require('./src/Generator');
 const Prando = require('prando');
 
 module.exports = (width, height, seed = Math.floor(Math.random() * Math.floor(100000)), algorithm = 'DEPTHFIRST') => {
-  const df = new Generator(width, height);
+  if (typeof width !== 'number' || typeof height !== 'number') {
+    throw new Error('Width and height must be numbers');
+  }
+  if (width === 0 | height === 0) {
+    throw new Error('Width and height must be greater than 0');
+  } else if (width > 3000 || height > 3000) {
+    throw new Error('Height and width must be a maximum of 3000');
+  }
+  const mazeGen = new Generator(width, height);
   const prando = new Prando(seed);
-  return df.generateMaze(algorithm, prando);
+  return mazeGen.generateMaze(algorithm, prando);
 };
