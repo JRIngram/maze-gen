bd0e1f91b3838a2043ff53ac317f3c037448fd4d
JRIngram <jr-ingram@hotmail.co.uk>
2021-10-16 16:23:27 +0100

    Add maze solver

--- src/Maze.js
+++ src/Maze.js
@@ -219,6 +219,98 @@ class Maze {
   }
 
   /**
+   * 
+   * @param {{row: number, column: number}} start the {row, column} coordinates of the starting cell
+   * @param {{row: number, column: number}} goal the {row, column} coordinates of the goal cell
+   */
+  findPath (start, goal) {
+    let openSet = [{...start, cost: 0,}];
+    let closedSet = [];
+
+    const calculateDistanceFromGoal = (currentCell, goalCell) => {
+      const {row, column} = currentCell;
+      const distanceToGoalRow = Math.abs(row - goalCell.row);
+      const distanceToGoalColumn = Math.abs(column - goalCell.column);
+      return distanceToGoalRow + distanceToGoalColumn;
+    };
+ 
+    const findAvailableCells = (currentCellInSet) => {
+      const {row, column, cost} = currentCellInSet;
+      const { walls } = this.cells[row][column];
+      const includedInSet = (set, row, column) => {
+        let cellInSet = false;
+        set.forEach((cell) => {
+          if(cell.row === row && cell.column === column){
+            cellInSet = true;
+          }
+        });
+        return cellInSet;
+      };
+
+      if(!walls.left){
+        const calulcatedColumn = column - 1;
+        if(!includedInSet(closedSet, row, calulcatedColumn) && !includedInSet(openSet, row, calulcatedColumn)){
+          const calculatedCost = cost + calculateDistanceFromGoal({row, column: calulcatedColumn}, goal);
+          openSet.push({row, column: calulcatedColumn, cost: calculatedCost, previousCell: currentCellInSet});
+        }
+      }
+      if(!walls.right){
+        const calulcatedColumn = column + 1;
+        if(!includedInSet(closedSet, row, calulcatedColumn) && !includedInSet(openSet, row, calulcatedColumn)){
+          const calculatedCost = cost + calculateDistanceFromGoal({row, column: calulcatedColumn}, goal);
+          openSet.push({row, column: calulcatedColumn, cost: calculatedCost, previousCell: currentCellInSet});
+        }
+      }
+      if(!walls.up){
+        const calculatedRow = row - 1;
+        if(!includedInSet(closedSet, calculatedRow, column) && !includedInSet(openSet, calculatedRow, column)){
+          const calculatedCost = cost + calculateDistanceFromGoal({row: calculatedRow, column}, goal);
+          openSet.push({row: calculatedRow, column: column, cost: calculatedCost, previousCell: currentCellInSet});
+        }
+      }
+      if(!walls.down){
+        const calculatedRow = row + 1;
+        if(!includedInSet(closedSet, calculatedRow, column) && !includedInSet(openSet, calculatedRow, column)){
+          const calculatedCost = cost + calculateDistanceFromGoal({row: calculatedRow, column}, goal);
+          openSet.push({row: calculatedRow, column: column, cost: calculatedCost, previousCell: currentCellInSet});
+        }
+      } 
+    }
+
+    let reachedGoal = false;
+    let foundGoalCell = null;
+    while(!reachedGoal){
+      if(openSet.length > 0){
+        findAvailableCells(openSet[0]);
+        closedSet.push(openSet[0]);
+        openSet.shift();
+        openSet.sort((cellOne, cellTwo) => {
+          return cellOne.cost - cellTwo.cost;
+        });
+        foundGoalCell = openSet.filter((openSetCell) => {
+          return openSetCell.column === goal.column && openSetCell.row === goal.row;
+        })[0]
+        if(foundGoalCell){
+          reachedGoal = true;
+        }
+      }
+    }
+
+    let path = [];
+    const constructPath = (cell) => {
+      const {row, column} = cell;
+      path.push({row, column});
+      if(cell.previousCell){
+        return constructPath(cell.previousCell);
+      }
+      path.reverse();
+    };
+    constructPath(foundGoalCell);
+
+    return calculateDistanceFromGoal(start, goal);
+  }
+
+  /**
      * @returns {string} The string represention of all cells within the maze.
      *  e.g.
      *  _ _ _
